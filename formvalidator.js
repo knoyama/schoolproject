5c467a24e5218523fad7263a103c7b55eace688a
knoyama <knoyama@wgu.edu>
2024-08-28 16:14:34 -1000

    Update formvalidator.js

--- formvalidator.js
+++ formvalidator.js
@@ -1,35 +1,8 @@
-function validateForm(event) {
-    let myForm = document.querySelector("#myForm");
-
-    const email=document.getElementById("email").value;
-    const reEmail=doucument.getElementById("reEmail"); 
-
-    myForm.email1.style.backgroundColor = "gray";
-    myForm.reEmail.style.backgroundColor = "gray";
-
-    if (myForm.email1.value == myForm.reEmail.value) {
-        myForm.email1.style.backgroundColor = "Green";
+function validateForm() {
+    let x = document.forms["myForm"]["email"].value;
+    let y = document.forms["myForm"]["reEmail"].value;
+        if (x !== y) {
+            window.prompt("Emails must match");
+            return false; 
+        }
     }
-
-    if (myForm.email1.value != myForm.reEmail.value) {
-        myForm.reEmail.style.backgroundColor = "Red"; 
-    }
-
-}
-let myForm = document.querySelector("#myForm");
-myForm.validate.addEventListener("click", validateForm);
-
-//document.getElementById("emailForm").addEventListener("submit", function(event) { 
-    //event.preventDefault(); 
-
-    //const email = document.getElementById("email").value;
-    //const confirmEmail= document.getElementByID("confirmEmail");
-    //const errorMessage = document.getElementById("errorMessage");
-    //if (email !== confirmEmail) {
-       //errorMessage.textContent = "Email Addresses do not match";
-    //}
-    //else {
-        //errorMessage.textContent = ""; 
-        //alert("Email addresses match. Form Submitted")
-    //}
-//})
